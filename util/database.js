888020a4f88fd0ab9e45b39191ed76a5b70ae5a2
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-06-27 00:48:16 +0530

    mysql

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Sukanta@1998'
+});
+
+module.exports = pool.promise();
